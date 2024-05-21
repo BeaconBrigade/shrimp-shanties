44cd497c41fd0a7193abc465f11d8293cf8afaa6
Ryan <beaconbrigade@gmail.com>
2024-05-21 15:45:50 -0230

    Fixed collision detection action

--- shrimp_shanties/game/check/input_timing.py
+++ shrimp_shanties/game/check/input_timing.py
@@ -3,6 +3,7 @@ from shrimp_shanties.game.player import Player, PLAYER_HIT_SPACE
 from shrimp_shanties.game.check import ActiveCheck
 from pygame.event import Event
 
+from shrimp_shanties.game.rhythm.note import Note
 
 INPUT_TIMING = next_event_id()
 
@@ -22,15 +23,14 @@ class InputTiming(ActiveCheck):
         nearest_item = None
         min_distance = float('inf')
         for entity in entity_list:
-            if hasattr(entity, 'is_descending') and entity.is_descending:
-                distance = abs(self.player.rect.y - entity.rect.y)
+            if isinstance(entity, Note):
+                distance = abs(self.player.pos.y - entity.height)
                 if distance < min_distance:
                     min_distance = distance
                     nearest_item = entity
         # Performs a collision check.
-        if nearest_item and self.player.rect.colliderect(nearest_item.rect):
+        if nearest_item and self.player.intersects_with(nearest_item.dimensions()):
             # Returns an event for increasing the score
             print("Collision detected with item:", nearest_item)
-            return Event(INPUT_TIMING, success=True, score=10.0, id=self.player.id)
-        print("checking for InputTiming")
+            return Event(INPUT_TIMING, success=True, score=1.0, id=self.player.id)
         return Event(INPUT_TIMING, success=False, score=0.0, id=self.player.id)
