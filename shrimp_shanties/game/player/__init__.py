7395707c9132831bb6489e47666befb41d1663f3
bigboi42069 <coltonwarford8@gmail.com>
2024-05-21 21:58:00 -0230

    Fixed judgement line

--- shrimp_shanties/game/player/__init__.py
+++ shrimp_shanties/game/player/__init__.py
@@ -15,9 +15,9 @@ class Player(Hitbox):
     def __init__(self, id=0):
         super().__init__(id)
         # Rect(left, top, width, height)
-        self.pos = Rect(WIDTH / 2 - 100, HEIGHT / 2 - 100, 200, 200)
-        self.sprite = AssetManager.load_texture("shrimp.png")
-        self.sprite = pygame.transform.scale(self.sprite, (200, 200))
+        self.pos = Rect(0, HEIGHT*4/5 - 100, 1000, 200)
+        self.sprite = AssetManager.load_texture("front-background.png")
+        self.sprite = pygame.transform.scale(self.sprite, (1000, 200))
 
     def dimensions(self) -> Rect:
         return self.pos
